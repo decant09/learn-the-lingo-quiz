35c39c48cd3463760eccbba616d707cfb4e959dc
Declan Caulfield <decant09@hotmail.com>
2023-04-22 15:58:44 +0000

    Add questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,30 @@ let allowingAnswers =  true;
 let scoreTotal = 0;
 let questionNumber = 0;
 let questionsPool = [];
+
+let questionsArray = [
+    {
+        question: '“Is the dug deid?” means',
+        answerA: 'Your trousers are too short',
+        answerB: 'Something terrible has happened',
+        answerC: 'Is the dog dead',
+        answerD: 'The flag is at half mast',
+        correct: 1
+    },
+    {
+        question: 'If someone asked for some juice while at the shop they would expect',
+        answerA: 'Alcohol',
+        answerB: 'Fruit juice',
+        answerC: 'Irn Bru',
+        answerD: 'Gossip',
+        correct: 3
+    },
+    {
+        question: 'Which of the following words does not mean girl',
+        answerA: 'Quine',
+        answerB: 'Hen',
+        answerC: 'Lassie',
+        answerD: 'Loon',
+        correct: 4
+    },
+]
