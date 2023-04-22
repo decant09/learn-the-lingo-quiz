8d623e1643a82c18e02f26b8c50d50bd722fe13f
Declan Caulfield <decant09@hotmail.com>
2023-04-22 15:42:26 +0000

    Create new variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
 const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answer-text"));
+
+let displayedQuestion = {};
+let allowingAnswers =  true;
+let scoreTotal = 0;
+let questionNumber = 0;
+let questionsPool = [];
