693e24b3fc127c11c42cf2ccb2d60ed0a9f78dc1
Declan Caulfield <decant09@hotmail.com>
2023-04-24 12:19:18 +0000

    Add get next question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answer-text"));
 
 let displayedQuestion = {};
-let allowingAnswers =  true;
+let allowingAnswers =  false;
 let scoreTotal = 0;
 let questionNumber = 0;
 let questionsPool = [];
@@ -10,42 +10,42 @@ let questionsPool = [];
 let questionsArray = [
     {
         question: '“Is the dug deid?” means',
-        answerA: 'Your trousers are too short',
-        answerB: 'Something terrible has happened',
-        answerC: 'Is the dog dead',
-        answerD: 'The flag is at half mast',
+        answer1: 'Your trousers are too short',
+        answer2: 'Something terrible has happened',
+        answer3: 'Is the dog dead',
+        answer4: 'The flag is at half mast',
         correct: 1
     },
     {
         question: 'If someone asked for some juice while at the shop they would expect',
-        answerA: 'Alcohol',
-        answerB: 'Fruit juice',
-        answerC: 'Irn Bru',
-        answerD: 'Gossip',
+        answer1: 'Alcohol',
+        answer2: 'Fruit juice',
+        answer3: 'Irn Bru',
+        answer4: 'Gossip',
         correct: 3
     },
     {
         question: 'Which of the following words does not mean girl',
-        answerA: 'Quine',
-        answerB: 'Hen',
-        answerC: 'Lassie',
-        answerD: 'Loon',
+        answer1: 'Quine',
+        answer2: 'Hen',
+        answer3: 'Lassie',
+        answer4: 'Loon',
         correct: 4
     },
     {
         question: '“Tea-time” is usually around',
-        answerA: '8am to 10am',
-        answerB: '11am to 1pm',
-        answerC: '3pm to 5pm',
-        answerD: '6pm to 8pm',
+        answer1: '8am to 10am',
+        answer2: '11am to 1pm',
+        answer3: '3pm to 5pm',
+        answer4: '6pm to 8pm',
         correct: 4
     },
     {
         question: 'If you were “away to do some messages”, you would be',
-        answerA: 'Doing some food shopping',
-        answerB: 'Writing postcards',
-        answerC: 'Checking your email',
-        answerD: 'Going on a date',
+        answer1: 'Doing some food shopping',
+        answer2: 'Writing postcards',
+        answer3: 'Checking your email',
+        answer4: 'Going on a date',
         correct: 1
     } 
 ]
@@ -58,5 +58,21 @@ function startQuiz() {
     questionsPool = [...questionsArray];
     getNextQuestion();
 }
+
+function getNextQuestion() {
+    questionNumber++;
+    const questionIndex = Math.floor(Math.random() * questionsPool.length);
+        displayedQuestion = questionsPool[questionIndex];
+        question.innerText = displayedQuestion.question;
+
+        answers.forEach(function(answer) {
+            const number = answer.dataset["option"];
+            answer.innerText = displayedQuestion["answer" + number];
+        });
+
+        questionsPool.splice(questionIndex, 1);
+
+        allowingAnswers =  true;
+};
 
 startQuiz();
