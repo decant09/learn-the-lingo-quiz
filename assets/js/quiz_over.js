5667b0e378d315853a8f35ebf1a3bdbb6748c6fd
Declan Caulfield <decant09@hotmail.com>
2023-04-25 15:23:52 +0000

    Get leaderbard data from local storage

--- assets/js/quiz_over.js
+++ assets/js/quiz_over.js
@@ -3,6 +3,9 @@ const save = document.getElementById('save');
 const currentPlayerResult = localStorage.getItem('currentPlayerResult');
 const scoreResult = document.getElementById('score-result');
 
+const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
+console.log(leaderboard);
+
 scoreResult.innerText = `You scored: ${currentPlayerResult}`;
 
 playerName.addEventListener('keyup', function() {
