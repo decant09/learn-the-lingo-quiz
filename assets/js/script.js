5dd07f9565f58823d7e6b111f9e5dbb67a7286cd
Declan Caulfield <decant09@hotmail.com>
2023-04-27 07:47:27 +0000

    Amend file path for quiz over page in getNextQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ function startQuiz() {
 function getNextQuestion() {
     if(questionsPool.length === 0 || questionNumber >= quizLength) {
         localStorage.setItem('currentPlayerResult', scoreTotal);
-        return window.location.assign("../../quiz_over.html");
+        return window.location.assign("../quiz_over.html");
     };
     questionNumber++;
     questionNumberValue.innerText = `${questionNumber}/${quizLength}`;
