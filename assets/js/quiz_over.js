3cdca90aee8d81d6dd04cb8a7303b96394d5c7d1
Declan Caulfield <decant09@hotmail.com>
2023-05-01 17:57:49 +0000

    Add result related message

--- assets/js/quiz_over.js
+++ assets/js/quiz_over.js
@@ -1,4 +1,17 @@
 const currentPlayerResult = localStorage.getItem('currentPlayerResult');
 const scoreResult = document.getElementById('score-result');
+const resultMessage = document.getElementById('message');
 
 scoreResult.innerText = `You scored: ${currentPlayerResult}`;
+
+function message() {
+    if(currentPlayerResult >= 100){
+        resultMessage.innerHTML = (`<h2>Well done!</h2>`);
+    } else if(currentPlayerResult >30 && currentPlayerResult < 100){
+        resultMessage.innerHTML = (`<h2>Could do better...</h2`);
+    } else{
+        resultMessage.innerHTML = (`<h2>Shocking!</h2>`);
+    }
+}
+
+message();
