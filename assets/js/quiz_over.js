d5d683cd487b57c622640250e0dd31a8cf7d01e1
Declan Caulfield <decant09@hotmail.com>
2023-05-01 17:03:46 +0000

    Remove leaderboard & hover[disabled], add home button

--- assets/js/quiz_over.js
+++ assets/js/quiz_over.js
@@ -1,31 +1,4 @@
-const playerName = document.getElementById('player-name');
-const save = document.getElementById('save');
 const currentPlayerResult = localStorage.getItem('currentPlayerResult');
 const scoreResult = document.getElementById('score-result');
 
-const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-
-const leaderboardDisplayMax = 3;
-
 scoreResult.innerText = `You scored: ${currentPlayerResult}`;
-
-playerName.addEventListener('keyup', function() {
-    save.disabled = !playerName.value
-})
-
-saveResult = function(e) {
-    e.preventDefault();
-
-    const result = {
-        result: currentPlayerResult,
-        name: playerName.value
-    };
-    leaderboard.push(result);
-    leaderboard.sort(function(a, b) {
-        return b.result - a.result;
-    });
-    leaderboard.splice(3);
-    
-    localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-    window.location.assign('index.html');
-};
