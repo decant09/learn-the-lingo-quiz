5d8afe6ff263985831c18ad9f93e8d1c250da9d6
decant09 <>
2023-05-03 16:42:08 +0000

    Update homepage text content & score result based  messages

--- assets/js/quiz_over.js
+++ assets/js/quiz_over.js
@@ -2,15 +2,15 @@ const currentPlayerResult = localStorage.getItem('currentPlayerResult');
 const scoreResult = document.getElementById('score-result');
 const resultMessage = document.getElementById('message');
 
-scoreResult.innerText = `You scored: ${currentPlayerResult}`;
+scoreResult.innerText = `You scored: ${currentPlayerResult}/100`;
 
 function message() {
     if(currentPlayerResult >= 100){
-        resultMessage.innerHTML = (`<h2>Well done!</h2>`);
-    } else if(currentPlayerResult >30 && currentPlayerResult < 100){
-        resultMessage.innerHTML = (`<h2>Could do better...</h2`);
+        resultMessage.innerHTML = (`<h2>Well done! You're a local!</h2>`);
+    } else if(currentPlayerResult >50 && currentPlayerResult < 100){
+        resultMessage.innerHTML = (`<h2>Good effort! Just a wee bit more practice!</h2`);
     } else{
-        resultMessage.innerHTML = (`<h2>Shocking!</h2>`);
+        resultMessage.innerHTML = (`<h2>Room to improve! Dinna worry, try again.</h2>`);
     }
 }
 
